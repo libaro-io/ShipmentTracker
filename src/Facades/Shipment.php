3117198b155b63dc565270fa5ae022d83d8e30e6
Johan Cleve <johan@libaro.be>
2022-06-29 15:36:22 +0200

    Removes dockblock

--- src/Facades/Shipment.php
+++ src/Facades/Shipment.php
@@ -4,9 +4,7 @@ namespace Libaro\ShipmentTracker\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @method static track(string $barCode): Status
- */
+
 class Shipment extends Facade
 {
     protected static function getFacadeAccessor(): string
