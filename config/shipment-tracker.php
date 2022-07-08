b7adc548fb5868d31373e6bd99cfd3f0ec8e06cf
Johan Cleve <johan@libaro.be>
2022-07-08 13:23:43 +0200

    Adds barcode tags for DHL

--- config/shipment-tracker.php
+++ config/shipment-tracker.php
@@ -28,7 +28,7 @@ return [
             'name' => 'dhl',
             'label' => 'DHL',
             'adapter' => \Libaro\ShipmentTracker\Adapters\DhlAdapter::class,
-            'barcode_tags' => ['JVGL'],
+            'barcode_tags' => ['JVGL', '(J)VGL', 'JJD', '(J)JD'],
             'credentials' => [
                 'api_key' => env('SHIPMENT_TRACKER_DHL_API_KEY'),
                 'api_secret' => env('SHIPMENT_TRACKER_DHL_API_SECRET')
