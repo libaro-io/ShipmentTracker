67d1c191c06368362e8a30b1627df513e6ebe22f
Johan Cleve <johan@libaro.be>
2022-07-08 12:18:06 +0200

    Updates the ProviderService

--- src/Services/ProviderService.php
+++ src/Services/ProviderService.php
@@ -2,42 +2,50 @@
 
 namespace Libaro\ShipmentTracker\Services;
 
+use Illuminate\Support\Collection;
 use Libaro\ShipmentTracker\Models\Provider;
 
 class ProviderService
 {
+    /**
+     * Returns a provider for the given name
+     *
+     * @param string $name
+     * @return Provider|null
+     */
     public static function getProviderByName(string $name): ?Provider
     {
-        $providers = self::getProviders();
-
-        $providerObject = null;
-
-        foreach ($providers as $provider) {
-            if ($provider['name'] === $name) {
-                $providerObject = self::makeProvider($provider);
-            }
-        }
-
-        return $providerObject;
+        return self::getProviders()->first(function($provider) use ($name) {
+            return $provider->name === $name;
+        });
     }
 
+    /**
+     * Returns a provider for the given barcode
+     *
+     * @param string $barcode
+     * @return Provider|null
+     */
     public static function getProviderByBarcode(string $barcode): ?Provider
     {
-        $providers = self::getProviders();
-
-        $providerObject = null;
-
-        foreach ($providers as $provider) {
-            foreach ($provider['barcode_tags'] as $tag) {
+        return self::getProviders()->first(function($provider) use ($barcode) {
+            $confirms = false;
+            foreach ($provider->barcodeTags as $tag) {
                 if (str_starts_with($barcode, $tag)) {
-                    $providerObject = self::makeProvider($provider);
+                    $confirms = true;
                 }
             }
-        }
 
-        return $providerObject;
+            return $confirms;
+        });
     }
 
+    /**
+     * Converts a provider array to an instance of a Provider class.
+     *
+     * @param array $provider
+     * @return Provider
+     */
     public static function makeProvider(array $provider): Provider
     {
         return (new Provider())
@@ -48,12 +56,50 @@ class ProviderService
             ->credentials($provider['credentials']);
     }
 
-    public static function getProviders()
+    /**
+     * Returns a collection of enabled providers.
+     * Each provider is an instance of the Provider class.
+     * Each instance contains: name, label, adapter class, barcode tags and credentials
+     *
+     * @return Collection
+     */
+    public static function getProviders(): Collection
     {
         $providers = config('shipment-tracker.providers');
 
-        return array_filter($providers, function ($provider) {
-            return $provider['enabled'];
-        });
+        $collection = new Collection();
+
+        foreach ($providers as $provider) {
+            if($provider['enabled']) {
+                $collection->add(self::makeProvider($provider));
+            }
+        }
+
+        return  $collection;
+    }
+
+    /**
+     * Returns an array of enabled providers.
+     * With only their name, label and barcode tags.
+     *
+     * @return array
+     */
+    public static function getPublicProviders(): array
+    {
+        $providers = config('shipment-tracker.providers');
+
+        $results = [];
+
+        foreach($providers as $provider) {
+            if($provider['enabled']) {
+                $results[] = [
+                    'name' => $provider['name'],
+                    'label' => $provider['label'],
+                    'barcode_tags' => $provider['barcode_tags']
+                ];
+            }
+        }
+
+        return $results;
     }
 }
