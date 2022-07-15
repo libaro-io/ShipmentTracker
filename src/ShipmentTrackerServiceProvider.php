da53afe9796b72d4ce35075eecc87799680646c4
Martijn Tetaert <martijn@libaro.be>
2022-07-15 16:40:02 +0200

    fix for publishing config

--- src/ShipmentTrackerServiceProvider.php
+++ src/ShipmentTrackerServiceProvider.php
@@ -9,11 +9,14 @@ class ShipmentTrackerServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/shipment-tracker.php', 'shipment-tracker');
+        $this->publishes([
+            __DIR__ . '/../config/shipment-tracker.php' => config_path('shipment-tracker.php'),
+        ], 'shipment-tracker-config');
     }
 
     public function register()
     {
+        $this->mergeConfigFrom(__DIR__ . '/../config/shipment-tracker.php', 'shipment-tracker-config');
         $this->app->bind('shipment', function ($app) {
             return new ShipmentService();
         });
